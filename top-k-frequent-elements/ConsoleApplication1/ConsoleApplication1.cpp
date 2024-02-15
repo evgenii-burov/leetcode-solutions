d74abd7c963c1cfd7f7059e160f347136421d56d
Евгений Буров <evgeny8584261@gmail.com>
2024-02-15 23:28:55 +0700

    start top k frequent elements

---
+++ top-k-frequent-elements/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include<vector>
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        for (int num : nums) {
+            mp[num]++;
+        }
+
+    }
+};
+
+int main() {
+    vector<int> nums = {1, 1, 1, 2, 2, 3};
+    int k = 2;
+    Solution sln;
+    for (auto elem : sln.topKFrequent(nums, k)) {
+        cout << elem << ', ';
+    }
+}
