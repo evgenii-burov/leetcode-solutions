45746a50f265058db515ef93dcf93b7166eaaae8
Евгений Буров <evgeny8584261@gmail.com>
2024-02-16 12:24:05 +0700

    added solution

--- top-k-frequent-elements/ConsoleApplication1/ConsoleApplication1.cpp
+++ top-k-frequent-elements/ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,11 +6,24 @@ using namespace std;
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
         unordered_map<int, int> mp;
         for (int num : nums) {
             mp[num]++;
         }
-
+        vector<vector<int>> bucket(n+1);
+        for (auto pair = mp.begin(); pair != mp.end(); pair++) {
+            bucket[pair->second].push_back(pair->first);
+        }
+        vector<int> top_frequent;
+        for (int i = n; i >= 0; i--) {
+            if (top_frequent.size() == k)
+                break;
+            if (!bucket[i].empty()) {
+                top_frequent.insert(top_frequent.end(), bucket[i].begin(), bucket[i].end());
+            }
+        }
+        return top_frequent;
     }
 };
 
@@ -18,7 +31,7 @@ int main() {
     vector<int> nums = {1, 1, 1, 2, 2, 3};
     int k = 2;
     Solution sln;
-    for (auto elem : sln.topKFrequent(nums, k)) {
+    for (int elem : sln.topKFrequent(nums, k)) {
         cout << elem << ', ';
     }
 }
